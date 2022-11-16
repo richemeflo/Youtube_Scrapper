bccb8e6d73e284828d216cdce71c116c8a93b88e
Florian Richeme <florian.richeme@eisti.fr>
2022-11-16 17:44:03 +0100

    youtube scrap

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,14 @@
-print("test")
+def setup_module(module):
+    print("\n--> Setup module")
+
+def teardown_module(module):
+    print("--> Teardown module")
+
+class TestClass:
+    @classmethod
+    def setup_class(cls):
+        print("--> Setup class")
+
+    @classmethod
+    def teardown_class(cls):
+        print("--> Teardown class")

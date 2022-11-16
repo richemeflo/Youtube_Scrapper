986b9aa3d46977d6c2510170ffdf25817583ab46
Florian Richeme <florian.richeme@eisti.fr>
2022-11-16 23:37:23 +0100

    clean une partie du code

--- tests/test_main.py
+++ tests/test_main.py
@@ -23,8 +23,8 @@ class TestClass:
         string_tmp = "On a pris notre temps mais voici enfin l'histoire du GP Explorer \u2764\ufe0f\nComme promis et pour votre plus grand bonheur, voici l'offre Deezer (0,99\u20ac par mois pendant 6 mois, puis 5,99\u20ac par mois pour tous les \u00e9tudiants) : https://DeezerFR.lnk.to/deezerxsqueezie\n\nCheck les derni\u00e8res pi\u00e8ces Yoko : http://www.yokoshop.com\n\nCollection Squeezie x RhinoShield : https://bit.ly/Squeezie-RhinoShield\n\nR\u00e9seaux :\n\nInsta : https://www.instagram.com/xsqueezie/\nTwitter : http://www.twitter.com/xSqueeZie\nFacebook : http://www.facebook.com/SqueeZiePageO...\n\nR\u00e8gles de l'espace commentaire (\u00e0 respecter sous peine d'\u00eatre banni par l'\u00e9quipe de mod\u00e9ration) :\n\n- \u00c9crire dans un fran\u00e7ais correct\n- Respecter les autres\n- Pas de commentaires type \"like si tu regardes la vid\u00e9o en chaussettes\"\n- Pas de pubs et de spams"
         assert self.res._get_description() == string_tmp
 
-    ##def test_links(self):
-        url = "https://scienceetonnante.com/2022/11/..."
+    def test_links(self):
+        url = "https://DeezerFR.lnk.to/deezerxsqueezie"
         assert url in self.res._get_links()
     
     def test_comments(self):
